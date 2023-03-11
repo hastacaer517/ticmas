3016027146995c998c31225c0ae7a88e0abd1bea
fabian <hastacaer517@gmail.com>
2023-03-11 01:25:28 -0300

    agregando mas js

--- js/vinculo.js
+++ js/vinculo.js
@@ -1,3 +1,5 @@
+alert("bienbenido/a click para entrar");
+
 document.getElementById("boton").onclick=function()
 { console.log("captura");
 document.getElementById("demo").innerHTML="SE HA ENVIADO JS"
